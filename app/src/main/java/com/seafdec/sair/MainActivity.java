6be83a06e56b2e23916ab0a6d7cfb51075fa3ea6
Elvi Nemiz <esnemiz@seafdec.org.ph>
2016-07-13 16:27:29 +0800

    Made opening of pdfs and email addresses work

--- app/src/main/java/com/seafdec/sair/MainActivity.java
+++ app/src/main/java/com/seafdec/sair/MainActivity.java
@@ -1,11 +1,18 @@
 package com.seafdec.sair;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -21,8 +28,31 @@ public class MainActivity extends Activity {
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setDisplayZoomControls(false);
         myWebView.setWebViewClient(new MyWebViewClient());
+        myWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+                request.setMimeType(mimeType);
+                //------------------------COOKIE!!------------------------
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                //------------------------COOKIE!!------------------------
+                request.addRequestHeader("User-Agent", userAgent);
+                request.setDescription("Downloading file...");
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/seafdec/sair/MyWebViewClient.java
+++ app/src/main/java/com/seafdec/sair/MyWebViewClient.java
@@ -1,6 +1,7 @@
 package com.seafdec.sair;
 
 import android.content.Intent;
+import android.net.MailTo;
 import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -8,9 +9,23 @@ import android.webkit.WebViewClient;
 public class MyWebViewClient extends WebViewClient {
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if (Uri.parse(url).getHost().endsWith("repository.seafdec.org.ph")) {
+        if(url.startsWith("mailto:")) {
+            MailTo mt = MailTo.parse(url);
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_EMAIL, new String[]{mt.getTo()});
+            i.putExtra(Intent.EXTRA_SUBJECT, mt.getSubject());
+            i.putExtra(Intent.EXTRA_CC, mt.getCc());
+            i.putExtra(Intent.EXTRA_TEXT, mt.getBody());
+            view.getContext().startActivity(i);
+            view.reload();
+            return true;
+        }
+
+            if (Uri.parse(url).getHost().endsWith("repository.seafdec.org.ph") || (Uri.parse(url).getHost().contains("hdl.handle.net".toLowerCase()))){
             return false;
         }
+
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         view.getContext().startActivity(intent);
         return true;
